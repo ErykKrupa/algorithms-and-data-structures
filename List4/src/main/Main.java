bcbfbe12e9e5296863626d3274aa7bee8e07a06f
Eryk <erykkrupa1@gmail.com>
2019-05-12 23:56:23 +0200

    Add main in first task from fourth list

---
+++ List4/src/main/Main.java
@@ -0,0 +1,89 @@
+package main;
+
+import tree.BinarySearchTree;
+
+import java.util.Scanner;
+
+public class Main {
+  private static int operations;
+  private static Scanner scanner = new Scanner(System.in);
+
+  private static int inputPositiveInteger() {
+    int positive;
+    while (true) {
+      try {
+        positive = Integer.parseInt(scanner.nextLine());
+        if (positive > 0) {
+          return positive;
+        }
+      } catch (NumberFormatException ignored) {
+      }
+      System.err.print("Once again: ");
+    }
+  }
+
+  private static void onceAgain() {
+    System.err.println("Once again: ");
+    operations--;
+  }
+
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.err.println("Two argument required");
+      return;
+    }
+    if (!(args[0].equals("--type")
+        && (args[1].equals("bst") || args[1].equals("rbt") || args[1].equals("splay")))) {
+      System.err.println("Type (--type) should be \"bst\", \"rbt\" or \"splay\"");
+      return;
+    }
+
+    BinarySearchTree tree = null;
+    if (args[1].equals("bst")) {
+      tree = new BinarySearchTree(String::compareTo);
+    } else if (args[1].equals("rbt")) {
+      //      tree = new RedBlackTree(String::compareTo);
+    } else {
+      //      tree = new SplayTree(String::compareTo);
+    }
+
+    System.out.print("Amount of operations: ");
+    operations = inputPositiveInteger();
+    for (; 0 < operations; operations--) {
+      String[] operation = scanner.nextLine().split(" ");
+      if (operation.length == 1) {
+        switch (operation[0]) {
+          case "empty":
+            System.out.println(tree.empty());
+            break;
+          case "inorder":
+            tree.inorder();
+            break;
+          default:
+            onceAgain();
+            break;
+        }
+      } else if (operation.length == 2) {
+        switch (operation[0]) {
+          case "insert":
+            tree.insert(operation[1]);
+            break;
+          case "delete":
+            tree.delete(operation[1]);
+            break;
+          case "search":
+            System.out.println(tree.search(operation[1]));
+            break;
+          case "load":
+            tree.load(operation[1]);
+            break;
+          default:
+            onceAgain();
+            break;
+        }
+      } else {
+        onceAgain();
+      }
+    }
+  }
+}
